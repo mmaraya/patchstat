28bb9d673d003c69730c42b6b8bbf1c8142fd00e
Mike Maraya <mike.maraya@gmail.com>
2016-12-19 22:11:52 -0500

    started code

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2016 Mike Maraya <mike[dot]maraya[at]gmail[dot]com>
+ * All rights reserved.
+ *
+ * This file is subject to the terms and conditions defined in
+ * https://github.com/mmaraya/patchstat/blob/master/LICENSE,
+ * which is part of this software package.
+ *
+ */
+
+#include <fstream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+}
