a6930ba3777659d7d4eecbf558e09f5e00699e0b
Mike Maraya <mike.maraya@gmail.com>
2017-01-03 16:37:43 -0500

    added queries

--- src/patchstat.sql
+++ src/patchstat.sql
@@ -3,6 +3,9 @@ SELECT DISTINCT NULL, Host from data_20161231 where Risk in ('Low', 'Medium', 'H
 DELETE FROM host;
 INSERT INTO host SELECT DISTINCT null, Host from data_20161231 where Risk in ('Low', 'Medium', 'High', 'Critical') and host not in (select ip_address from host);
 INSERT OR REPLACE INTO vulnerability SELECT PluginID, Risk, CVE, CVSS from data_20161231 where Risk in ('Low', 'Medium', 'High', 'Critical');
+
+-- create scan_result entries
+INSERT OR REPLACE INTO scan_result 
 SELECT scan.id AS scan_id,
        host.id AS host_id, 
        vulnerability.id AS vulnerability_id 
@@ -14,4 +17,20 @@ SELECT scan.id AS scan_id,
    AND vulnerability.id = data_20161231.PluginID
    AND scan.network_id = 1
    AND scan.scan_date = "20161231"
+
+-- hosts with vulnerabilities
+SELECT network.network_name, 
+       scan.scan_date,
+       host.ip_address,
+       scan_result.vulnerability_id, 
+       vulnerability.severity
+  FROM scan_result,
+       scan,
+       network,
+       host,
+       vulnerability
+ WHERE host.id = scan_result.host_id
+   AND scan_result.scan_id = scan.id
+   AND network.id = scan.network_id
+   AND vulnerability.id = scan_result.vulnerability_id
 
