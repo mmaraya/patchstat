ee8bd3834b6c94a3a4712a3ccb55a1c252be01b7
Mike Maraya <mike.maraya@gmail.com>
2017-01-05 19:32:34 -0500

    load second set of data

--- src/patchstat.sql
+++ src/patchstat.sql
@@ -47,3 +47,24 @@ SELECT network.network_name,
    AND network.id = scan.network_id
    AND vulnerability.id = scan_result.vulnerability_id
 GROUP BY severity
+
+-- load 1/3/17 data
+INSERT INTO scan SELECT null, network.id, '20170103' FROM network WHERE network_name = 'home'
+INSERT INTO host SELECT DISTINCT null, Host from data_20170103 where Risk in ('Low', 'Medium', 'High', 'Critical') and host not in (select ip_address from host);
+INSERT OR REPLACE INTO vulnerability SELECT PluginID, Risk, CVE, CVSS from data_20170103 where Risk in ('Low', 'Medium', 'High', 'Critical')
+INSERT OR REPLACE INTO scan_result 
+SELECT scan.id AS scan_id,
+       host.id AS host_id, 
+       vulnerability.id AS vulnerability_id 
+  FROM data_20170103 AS data, 
+       scan,
+       host, 
+       network, 
+       vulnerability 
+ WHERE host.ip_address = data.host
+   AND vulnerability.id = data.PluginID
+   AND scan.network_id = network.id 
+   AND scan.scan_date = "20170103"
+   AND network.network_name = "home"
+
+
