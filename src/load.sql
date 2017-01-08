aad02cd403707f8b6e9a3e93dd9cc9cdef79a59c
Mike Maraya <mike.maraya@gmail.com>
2017-01-08 11:17:21 -0500

    fixed errors

--- src/load.sql
+++ src/load.sql
@@ -1,12 +1,9 @@
 -- add network
-BEGIN; 
 INSERT INTO network VALUES (null, 'home');
-COMMIT;
 
 -- load 12/31/16 data
-BEGIN;
 INSERT INTO scan SELECT null, network.id, '20161231' FROM network WHERE network_name = 'home';
-INSERT INTO host SELECT DISTINCT null, Host from data_20170103 where Risk in ('Low', 'Medium', 'High', 'Critical') and host not in (select ip_address from host);
+INSERT INTO host SELECT DISTINCT null, Host from data_20161231 where Risk in ('Low', 'Medium', 'High', 'Critical') and host not in (select ip_address from host);
 INSERT OR REPLACE INTO vulnerability SELECT PluginID, Risk, CVE, CVSS from data_20161231 where Risk in ('Low', 'Medium', 'High', 'Critical');
 INSERT OR REPLACE INTO scan_result 
 SELECT scan.id AS scan_id,
@@ -22,13 +19,11 @@ SELECT scan.id AS scan_id,
    AND scan.network_id = network.id 
    AND scan.scan_date = "20161231"
    AND network.network_name = "home";
-COMMIT;
 
 -- load 1/3/17 data
-BEGIN;
-INSERT INTO scan SELECT null, network.id, '20170103' FROM network WHERE network_name = 'home'
+INSERT INTO scan SELECT null, network.id, '20170103' FROM network WHERE network_name = 'home';
 INSERT INTO host SELECT DISTINCT null, Host from data_20170103 where Risk in ('Low', 'Medium', 'High', 'Critical') and host not in (select ip_address from host);
-INSERT OR REPLACE INTO vulnerability SELECT PluginID, Risk, CVE, CVSS from data_20170103 where Risk in ('Low', 'Medium', 'High', 'Critical')
+INSERT OR REPLACE INTO vulnerability SELECT PluginID, Risk, CVE, CVSS from data_20170103 where Risk in ('Low', 'Medium', 'High', 'Critical');
 INSERT OR REPLACE INTO scan_result 
 SELECT scan.id AS scan_id,
        host.id AS host_id, 
@@ -43,18 +38,16 @@ SELECT scan.id AS scan_id,
    AND scan.network_id = network.id 
    AND scan.scan_date = "20170103"
    AND network.network_name = "home";
-COMMIT;
 
--- load 1/4/17 data
-BEGIN;
-INSERT INTO scan SELECT null, network.id, '20170104' FROM network WHERE network_name = 'home'
-INSERT INTO host SELECT DISTINCT null, Host from data_20170104 where Risk in ('Low', 'Medium', 'High', 'Critical') and host not in (select ip_address from host);
-INSERT OR REPLACE INTO vulnerability SELECT PluginID, Risk, CVE, CVSS from data_20170104 where Risk in ('Low', 'Medium', 'High', 'Critical')
+-- load 1/5/17 data
+INSERT INTO scan SELECT null, network.id, '20170105' FROM network WHERE network_name = 'home';
+INSERT INTO host SELECT DISTINCT null, Host from data_20170105 where Risk in ('Low', 'Medium', 'High', 'Critical') and host not in (select ip_address from host);
+INSERT OR REPLACE INTO vulnerability SELECT PluginID, Risk, CVE, CVSS from data_20170105 where Risk in ('Low', 'Medium', 'High', 'Critical');
 INSERT OR REPLACE INTO scan_result 
 SELECT scan.id AS scan_id,
        host.id AS host_id, 
        vulnerability.id AS vulnerability_id 
-  FROM data_20170104 AS data, 
+  FROM data_20170105 AS data, 
        scan,
        host, 
        network, 
@@ -62,7 +55,6 @@ SELECT scan.id AS scan_id,
  WHERE host.ip_address = data.host
    AND vulnerability.id = data.PluginID
    AND scan.network_id = network.id 
-   AND scan.scan_date = "20170104"
+   AND scan.scan_date = "20170105"
    AND network.network_name = "home";
-COMMIT;
 
