c7cf38d81ada738e12666f407be2067f5445ac9f
Mike Maraya <mike.maraya@gmail.com>
2017-01-08 14:59:26 -0500

    count vulnerabilities remediated

--- src/reports.sql
+++ src/reports.sql
@@ -1,7 +1,7 @@
 -- count all current and past vulnerabilities by severity
 SELECT network_name, severity, vulns FROM
 (
-    SELECT  network.network_name
+    SELECT  network.network_name AS network
            ,scan_date
            ,vulnerability.severity
            ,count(*) AS vulns
@@ -18,7 +18,7 @@ SELECT network_name, severity, vulns FROM
 GROUP BY network_name, severity;
 
 -- count all current vulnerabilities by severity
-  SELECT  network.network_name
+  SELECT  network.network_name AS network
          ,vulnerability.severity
          ,count(*) AS vulns
     FROM  scan 
@@ -28,6 +28,12 @@ GROUP BY network_name, severity;
    WHERE  scan_result.scan_id = scan.id
      AND  network.id = scan.network_id
      AND  vulnerability.id = scan_result.vulnerability_id
-     AND  scan.id = (SELECT MAX(id) FROM scan)
+     AND  scan.id = (SELECT id FROM scan ORDER BY scan_date DESC LIMIT 1)
 GROUP BY network_name, severity;
 
+-- count all vulnerabilities remediated by severity
+  SELECT  A.network_name AS network
+         ,A.severity
+         ,A.vulns - COALESCE(B.vulns, 0) AS remediated
+    FROM  vulnerabilities_all A LEFT JOIN vulnerabilities_current B 
+      ON (A.network_name = B.network_name AND A.severity = B.severity);
